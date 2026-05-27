91d380f73a9f2067f84850bbd229cfc9a04021a8
Caleb Asia <calebasia1811@gmail.com>
2026-05-27 12:18:51 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,10 @@
+function greet(name) {
+    return "Hello", name
+}
+function add(num1,num2) {
+    return num1 + num2 
+}
+let greeting= greet("Bartholomew")
+console.log(greeting)
+let sum = add(25,36);
+console.log(sum)
